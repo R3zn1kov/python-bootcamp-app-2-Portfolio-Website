f239a1e8a6cd3f7b9679ca5377557f7b66991064
R3zn1kov <ldlreznikov@gmail.com>
2024-02-06 23:54:36 +0100

    Add text on a new column

--- homepageapp2.py
+++ homepageapp2.py
@@ -1,17 +1,21 @@
 import streamlit as st
 
+st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
-
+col3 = st.columns(1)[0]
 with col1:
     st.image("images/photo.jpg")
 
 with col2:
     st.title("Luigi di Lillo")
     content = """" 
-    Ciao! sono Luigi, junior marketing specialist,
+    Ciao! sono Luigi, Junior Marketing Specialist,
     appassionato di data analysis e questo è uno dei 
     progetti relativi alla mia roadmap per diventare
     prolifico nell'utilizzo di Python. Scorrendo in basso potrai trovare
     tutti i link ai progetti che sto sviluppando per allenarmi
     """
-    st.write(content)
+    st.info(content)
+with col3:
+    information = "Ajeje brazorf lorem ipsum dolor amet"
+    st.write(information)
